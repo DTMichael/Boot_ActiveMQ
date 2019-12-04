be2db53f2ccac8430e6e0104a9e7f073a81533c6
DTMichael <Zhuyi2632@163.com>
2019-12-04 23:28:08 +0800

    首次提交

---
+++ src/main/java/com/atguigu/boot_activemq/produce/Queue_Produce.java
@@ -0,0 +1,31 @@
+package com.atguigu.boot_activemq.produce;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Queue;
+import java.util.UUID;
+
+@Component
+public class Queue_Produce {
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    public void produceMsg()
+    {
+        jmsMessagingTemplate.convertAndSend(queue,"*******"+ UUID.randomUUID().toString().substring(0,6));
+        System.out.println("*******触发投递");
+    }
+
+    @Scheduled(fixedDelay = 3000)
+    public void produceMsgScheduled()
+    {
+        jmsMessagingTemplate.convertAndSend(queue,"********"+UUID.randomUUID().toString().substring(0,10));
+        System.out.println("定时 send  OK");
+    }
+}
---
+++ src/test/java/com/atguigu/boot_activemq/BootActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot_activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootActivemqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
